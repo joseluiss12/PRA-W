7d726ca936aa757d369b8fdd347ea341d5bf5578
joseluiss12 <146230578+joseluiss12@users.noreply.github.com>
2023-11-29 18:44:52 +0100

    Add files via upload

---
+++ PRA_2324_P3/TableEntry.h
@@ -0,0 +1,61 @@
+#ifndef TABLEENTRY_H
+#define TABLEENTRY_H
+
+#include <string>
+#include <ostream>
+
+template <typename V> 
+class TableEntry {
+	
+	public:
+        
+		std::string key;
+		V value;
+
+		TableEntry(std::string key, V value){
+		
+			this->key = key;
+			this->value = value;
+
+		}
+
+		TableEntry(std::string key){
+		
+			this->key = key;
+			
+		}
+
+		TableEntry(){
+		
+			this->key = "";
+
+		}
+
+		friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
+
+			if(te1.key == te2.key)
+				return true;
+			else
+				return false;
+			
+		}
+
+		friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
+
+			if(te1.key != te2.key)
+				return true;
+			else 
+				return false;
+
+		}
+		friend std::ostream&operator<<(std::ostream &out, const TableEntry<V> &te){
+
+			out << "Key = " << te.key << " -> value = " << te.value << std::endl;
+			return out;
+
+		}
+
+    
+};
+
+#endif
